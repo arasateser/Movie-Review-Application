bc6e0cbb2cf30686600232ca355abb17c52f30c1
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-03-08 22:32:49 +0300

    CrossOrigin update for frontend implementation

--- src/main/java/com/arasreviews/movies/MoviesApplication.java
+++ src/main/java/com/arasreviews/movies/MoviesApplication.java
@@ -2,9 +2,11 @@ package com.arasreviews.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 @RestController
 public class MoviesApplication {
