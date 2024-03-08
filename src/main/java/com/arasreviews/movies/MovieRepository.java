18a4b79491686cf0c23151ba9b7d23b40bd1f224
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2024-03-08 17:23:06 +0300

    controller\repo\service

---
+++ src/main/java/com/arasreviews/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.arasreviews.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> getMovieByImdbId(String imdbId);
+
+}
---
+++ src/main/java/com/arasreviews/movies/Review.java
@@ -0,0 +1,16 @@
+package com.arasreviews.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id; //which review
+    private String body; //review message
+}
